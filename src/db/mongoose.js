282fc7ce0b95e227546b923c2fbdc24007cb9ab1
Tarun Pritam R <52533898+rodriguezmatirp@users.noreply.github.com>
2020-04-19 20:57:23 +0530

    Init commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
+mongoose.Promise = global.Promise
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
